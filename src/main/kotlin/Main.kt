a029de0c785414e3d181e6e834c2d46dad61d021
david <fearlessiix563@gmail.com>
2021-11-23 00:21:07 -0500

    Added some examples of health to Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,4 +9,10 @@ fun main(args: Array<String>) {
     player.gainXp(14235)
     println("\nAfter gaining 14235 xp\n")
     println(player)
+    player.takeDamage(15)
+    println("\nAfter losing 15 health\n")
+    println(player)
+    player.gainHealth(53)
+    println("\nAfter gaining 53 health\n")
+    println(player)
 }
